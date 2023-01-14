6b8d3edd89815bf8d6ac3fcdb1773279a1140eca
Pham Tran Tuan <Ptuankhang@gmail.com>
2023-01-14 14:26:30 +0700

    Update install-devops.sh

--- install-devops.sh
+++ install-devops.sh
@@ -18,7 +18,7 @@ wget -qO- https://get.docker.com/ | sh
 
 # Install docker-compose
 COMPOSE_VERSION=`git ls-remote https://github.com/docker/compose | grep refs/tags | grep -oP "[0-9]+\.[0-9][0-9]+\.[0-9]+$" | tail -n 1`
-sudo sh -c "curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
+sudo sh -c "curl -L https://github.com/docker/compose/releases/download/v${COMPOSE_VERSION}/docker-compose-$(echo $(uname -s) | tr '[:upper:]' '[:lower:]')-`uname -m` > /usr/local/bin/docker-compose"
 sudo chmod +x /usr/local/bin/docker-compose
 sudo sh -c "curl -L https://raw.githubusercontent.com/docker/compose/${COMPOSE_VERSION}/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose"
 
